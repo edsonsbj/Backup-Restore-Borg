e1d7d67603846192c1c61b23a27a01cc49a79f14
edsonsbj <edson_silvasp@hotmail.com>
2023-10-29 12:34:37 -0300

    Update

--- setup.sh
+++ setup.sh
@@ -4,7 +4,8 @@
 #################################### Pre defined variables ##################################
 #############################################################################################
 
-MountPoint='/mnt/nextcloud_backup'
+BackupDIsk='/mnt/nextcloud_backup'
+Borg_repo='$BackupDIsk/Borg'
 BackupRestoreConf='BackupRestore.conf'
 LogFile='/var/log/Rsync-$(date +%Y-%m-%d_%H-%M).txt'
 SourceDir='/'
@@ -90,64 +91,11 @@ disk_backup() {
   clear
 
   echo "Enter the backup drive mount point here."
-  echo "Default: ${MountPoint}"
+  echo "Default: ${BackupDIsk}"
   echo ""
-  read -p "Enter a directory or press ENTER if the backup directory is ${MountPoint}: " MOUNTPOINT
+  read -p "Enter a directory or press ENTER if the backup directory is ${BackupDIsk}: " BACKUPDISK
 
-  [ -z "$MOUNTPOINT" ] ||  MountPoint=$MOUNTPOINT
-
-  clear
-  }
-
-  # Function to BORG
-borg() {
-  echo "Enter the BORG_REPO here."
-  echo "Default: ${Borg_repo}"
-  echo ""
-  read -p "Enter a directory or press ENTER if the backup directory is ${Borg_repo}: " BORG_REPO
-
-  [ -z "$BORG_REPO" ] || Borg_repo=$BORG_REPO
-
-  # Ask the user for the Borg repository password
-  echo "Enter the Borg repository password:"
-  read -s BORG_PASS
-
-  # Ask the user if they want to encrypt the password
-  echo "Do you want to encrypt the password? (y/n)"
-  read ANSWER
-
-if [ "$ANSWER" = "y" ]; then
-  # Save the password in a file called 'pass'
-  echo $BORG_PASS > pass
-
-  # Encrypt the file using openssl
-  openssl enc -e -aes-256-cbc -a -md sha512 -pbkdf2 -iter 1000 -salt -in "pass" > "pass.enc" -pass pass:key
-
-  # Set the openssl decryption command in the BorgPassCommand variable
-  PASSCOMMAND="openssl enc -d -aes-256-cbc -a -md sha512 -pbkdf2 -iter 1000 -salt -in pass.enc -pass pass:key"
-
-  echo "The password has been encrypted and saved in the 'pass.enc' file."
-
-  rm pass
-else
-  # Save the password in the BorgPassphrase variable
-  BORG_PASSPHRASE=$BORG_PASS
-
-  echo "The password has been saved in the 'BORG_PASSPHRASE' environment variable."
-fi
-
-  clear
-
-}
-
-# Function to backup
-backup() {
-  echo "Enter the folder you want to backup here."
-  echo "Default: ${SourceDir}"
-  echo ""
-  read -p "Enter a directory or press ENTER if the backup directory is ${SourceDir}:" SOURCEDIR
-
-  [ -z "$SOURCEDIR" ] || SourceDir=$SOURCEDIR
+  [ -z "$BACKUPDISK" ] ||  BackupDIsk=$BACKUPDISK
 
   clear
 
@@ -161,19 +109,7 @@ backup() {
 uuid='$uuid'
 
 # TODO: The Backup Drive Mount Point
-MountPoint='$MountPoint'
-
-# TODO: Borg Repository
-export BORG_REPO='$Borg_repo'
-
-# TODO: The Command to capture password
-export BORG_PASSCOMMAND="$PASSCOMMAND"
-
-# TODO: The password Borg
-export BORG_PASSPHRASE="$BORG_PASSPHRASE"
-
-# TODO: Directory to backup 
-SourceDir='$SourceDir'
+BackupDisk='$BackupDisk'
 
 # Log File
 LogFile="$LogFile"
@@ -182,13 +118,74 @@ EOF
 
   clear
 
-  wget https://raw.githubusercontent.com/edsonsbj/Backup-Restore/main/Backup.sh
-  wget https://raw.githubusercontent.com/edsonsbj/Backup-Restore/main/Restore.sh
+}
 
-  chmod 700 *.sh
+# Function to BORG
+borg() {
+  # Creation of the Directorate where the secrets is stored  
+  mkdir -p $HOME.Secrets
+  cd $HOME/.Secrets
+
+  echo "Enter the BORG_REPO here."
+  echo "Default: ${Borg_repo}"
+  echo ""
+  read -p "Enter a directory or press ENTER if the backup directory is ${Borg_repo}: " BORG_REPO
+
+  [ -z "$BORG_REPO" ] || Borg_repo=$BORG_REPO
+
+  # Ask the user for the Borg repository password
+  echo "Enter the Borg repository password:"
+  read -s BORG_PASS
+
+  # Save the password in a file called 'pass'
+  echo $BORG_PASS > $Passfile
+
+  # Generate random key
+  Keyfile="/path/to/keyfile"
+  Key=$(openssl rand -out $Keyfile -base64 4096)
+
+  # Encrypt the file using openssl
+  PassfileEncr="/path/to/encrypted/passfile"
+  openssl enc -e -aes-256-cbc -a -md sha512 -pbkdf2 -iter 10000 -salt -in "$Passfile" > "$PassfileEncr" -pass file:"$Keyfile"
+
+  # Set the openssl decryption command in the BorgPassCommand variable
+  PASSCOMMAND="openssl enc -d -aes-256-cbc -a -md sha512 -pbkdf2 -iter 10000 -salt -in "$PassfileEncr" -pass file:"$Keyfile""
+
+  echo "The password has been encrypted and saved in the file."
+
+  rm $Passfile
 
   clear
 
+  tee -a ./"${BackupRestoreConf}" <<EOF
+# TODO: Borg Repository
+export BORG_REPO='$Borg_repo'
+
+# TODO: The Command to capture password
+export BORG_PASSCOMMAND="$PASSCOMMAND"
+
+EOF
+  clear
+}
+
+# Function to backup
+backup() {
+  echo "Enter the folder you want to backup here."
+  echo "Default: ${SourceDir}"
+  echo ""
+  read -p "Enter a directory or press ENTER if the backup directory is ${SourceDir}:" SOURCEDIR
+
+  [ -z "$SOURCEDIR" ] || SourceDir=$SOURCEDIR
+
+  clear
+
+  tee -a ./"${BackupRestoreConf}" <<EOF
+# TODO: Directory to backup 
+SourceDir='$SourceDir'
+
+EOF
+
+  clear
 }
 
 # Function to backup Nextcloud settings
@@ -217,27 +214,7 @@ nextcloud() {
     
   clear
 
-  mkdir -p $HOME/Scripts
-  cd $HOME/Scripts
-
   tee -a ./"${BackupRestoreConf}" <<EOF
-# Configuration for Backup-Restore scripts
-
-# TODO: The uuid of the backup drive
-uuid='$uuid'
-
-# TODO: The Backup Drive Mount Point
-MountPoint='$MountPoint'
-
-# TODO: Borg Repository
-export BORG_REPO='$Borg_repo'
-
-# TODO: The Command to capture password
-export BORG_PASSCOMMAND="$PASSCOMMAND"
-
-# TODO: The password Borg
-export BORG_PASSPHRASE="$BORG_PASSPHRASE"
-
 # TODO: The service name of the web server. Used to start/stop web server (e.g. 'systemctl start <webserverServiceName>')
 webserverServiceName='$webserverServiceName'
 
@@ -262,17 +239,7 @@ DBUser='$DBUser'
 # TODO: The password of the Nextcloud database user
 DBPassword='$DBPassword'
 
-# Log File
-LogFile="$LogFile"
-
 EOF
-
-  clear
-
-  wget https://raw.githubusercontent.com/edsonsbj/Backup-Restore/main/scripts/Nextcloud/Backup.sh
-  wget https://raw.githubusercontent.com/edsonsbj/Backup-Restore/main/scripts/Nextcloud/Restore.sh
-
-  chmod 700 *.sh
 
   clear
 }
@@ -304,7 +271,7 @@ clear
 }
 
 # Function to configure the media server
-configure_mediaserver() {
+mediaserver() {
   echo "Choose the media server for backup:"
   select media_server in "Emby" "Jellyfin" "Plex"; do
     case $media_server in
@@ -361,27 +328,7 @@ configure_mediaserver() {
 
   clear 
 
-  mkdir -p $HOME/Scripts
-  cd $HOME/Scripts
-
   tee -a ./"${BackupRestoreConf}" <<EOF
-# Configuration for Backup-Restore scripts
-
-# TODO: The uuid of the backup drive
-uuid='$uuid'
-
-# TODO: The Backup Drive Mount Point
-MountPoint='$MountPoint'
-
-# TODO: Borg Repository
-export BORG_REPO='$Borg_repo'
-
-# TODO: The Command to capture password
-export BORG_PASSCOMMAND="$PASSCOMMAND"
-
-# TODO: The password Borg
-export BORG_PASSPHRASE="$BORG_PASSPHRASE"
-
 # TODO: The service name of the media server. Used to start/stop web server (e.g. 'systemctl start <mediaserverServiceName>')
 MediaserverService='$MediaserverService'
 
@@ -391,170 +338,9 @@ MediaserverUser='$MediaserverUser'
 # TODO: The directory where the Media Server settings are stored (this directory is stored within /var/lib)
 MediaserverConf='$MediaserverConf'
 
-# Log File
-LogFile="$LogFile"
-
 EOF
 
   clear
-
-  wget https://raw.githubusercontent.com/edsonsbj/Backup-Restore/main/scripts/Media%20Server/Backup.sh
-  wget https://raw.githubusercontent.com/edsonsbj/Backup-Restore/main/scripts/Media%20Server/Restore.sh
-
-  chmod 700 *.sh
-
-  clear
-
-}
-
-# Function to configure the media server with NextCloud
-configure_nextcloud_mediaserver() {
-  echo "Enter the path to the Nextcloud file directory."
-  echo "Usually: ${NextcloudConfig}"
-  echo ""
-  read -p "Enter a directory or press ENTER if the file directory is ${NextcloudConfig}: " NEXTCLOUDCONF
-
-  [ -z "$NEXTCLOUDCONF" ] ||  NextcloudConfig=$NEXTCLOUDCONF
-  clear
-
-  echo "Enter the webserver service name."
-  echo "Usually: nginx or apache2"
-  echo ""
-  read -p "Enter an new webserver service name or press ENTER if the webserver service name is ${webserverServiceName}: " WEBSERVERSERVICENAME
-
-  [ -z "$WEBSERVERSERVICENAME" ] ||  webserverServiceName=$WEBSERVERSERVICENAME
-  clear
-
-  NextcloudDataDir=$(sudo -u www-data php $NextcloudConfig/occ config:system:get datadirectory)
-  DatabaseSystem=$(sudo -u www-data php $NextcloudConfig/occ config:system:get dbtype)
-  NextcloudDatabase=$(sudo -u www-data php $NextcloudConfig/occ config:system:get dbname)
-  DBUser=$(sudo -u www-data php $NextcloudConfig/occ config:system:get dbuser)
-  DBPassword=$(sudo -u www-data php $NextcloudConfig/occ config:system:get dbpassword)
-
-  echo "Choose the Backup Media Server: "
-  select media_server in "Emby" "Jellyfin" "Plex"; do
-    case $media_server in
-      "Emby")
-        MediaserverService="emby"
-        MediaserverUser="emby"
-
-        echo "Enter the path to the Emby file directory."
-        echo "Usually: ${Emby_Conf}"
-        echo ""
-        read -p "Enter a directory or press ENTER if the file directory is ${Emby_Conf}: " EMBY_CONF
-
-        [ -z "$EMBY_CONF" ] ||  Emby_Conf=$EMBY_CONF
-        clear
-
-        MediaserverConf="$Emby_Conf"
-        break
-        ;;
-      "Jellyfin")
-        MediaserverService="jellyfin"
-        MediaserverUser="jellyfin"
-
-        echo "Enter the path to the Jellyfin file directory."
-        echo "Usually: ${Jellyfin_Conf}"
-        echo ""
-        read -p "Enter a directory or press ENTER if the file directory is ${Jellyfin_Conf}: " JELLYFIN_CONF
-
-        [ -z "$JELLYFIN_CONF" ] ||  Jellyfin_Conf=$JELLYFIN_CONF
-        clear
-
-        MediaserverConf="$Jellyfin_Conf"
-        break
-        ;;
-      "Plex")
-        MediaserverService="plexmediaserver"
-        MediaserverUser="plex"
-
-        echo "Enter the path to the Jellyfin file directory."
-        echo "Usually: ${Plex_Conf}"
-        echo ""
-        read -p "Enter a directory or press ENTER if the file directory is ${Plex_Conf}: " PLEX_CONF
-
-        [ -z "$PLEX_CONF" ] ||  Plex_Conf=$PLEX_CONF
-        clear
-
-        MediaserverConf="$Plex_Conf"
-        break
-        ;;
-      *)
-        echo "Invalid option, try again."
-        ;;
-    esac
-  done
-
-  clear 
-
-  mkdir -p $HOME/Scripts
-  cd $HOME/Scripts
-
-  tee -a ./"${BackupRestoreConf}" <<EOF
-# Configuration for Backup-Restore scripts
-
-# TODO: The uuid of the backup drive
-uuid='$uuid'
-
-# TODO: The Backup Drive Mount Point
-MountPoint='$MountPoint'
-
-# TODO: Borg Repository
-export BORG_REPO='$Borg_repo'
-
-# TODO: The Command to capture password
-export BORG_PASSCOMMAND="$PASSCOMMAND"
-
-# TODO: The password Borg
-export BORG_PASSPHRASE="$BORG_PASSPHRASE"
-
-# TODO: The service name of the web server. Used to start/stop web server (e.g. 'systemctl start <webserverServiceName>')
-webserverServiceName='$webserverServiceName'
-
-# TODO: The directory of your Nextcloud installation (this is a directory under your web root)
-NextcloudConfig='$NextcloudConfig'
-
-# TODO: The directory of your Nextcloud data directory (outside the Nextcloud file directory)
-# If your data directory is located in the Nextcloud files directory (somewhere in the web root),
-# the data directory must not be a separate part of the backup
-NextcloudDataDir='$NextcloudDataDir'
-
-# TODO: The name of the database system (one of: mysql, mariadb, postgresql)
-# 'mysql' and 'mariadb' are equivalent, so when using 'mariadb', you could also set this variable to 'mysql' and vice versa.
-DatabaseSystem='$DatabaseSystem'
-
-# TODO: Your Nextcloud database name
-NextcloudDatabase='$NextcloudDatabase'
-
-# TODO: Your Nextcloud database user
-DBUser='$DBUser'
-
-# TODO: The password of the Nextcloud database user
-DBPassword='$DBPassword'
-
-# TODO: The service name of the media server. Used to start/stop web server (e.g. 'systemctl start <mediaserverServiceName>')
-MediaserverService='$MediaserverService'
-
-# TODO: The service name of the media server. Used to restore permissions media server settings)
-MediaserverUser='$MediaserverUser'
-
-# TODO: The directory where the Media Server settings are stored (this directory is stored within /var/lib)
-MediaserverConf='$MediaserverConf'
-
-# Log File
-LogFile="$LogFile"
-
-EOF
-
-  clear
-
-  wget https://raw.githubusercontent.com/edsonsbj/Backup-Restore/main/scripts/Nextcloud%20%2B%20Media%20server%20/Backup.sh
-  wget https://raw.githubusercontent.com/edsonsbj/Backup-Restore/main/scripts/Nextcloud%20%2B%20Media%20server%20/Restore.sh
-  
-  chmod 700 *.sh
-
-  clear
-
 }
 
 # Main menu
@@ -573,24 +359,41 @@ while true; do
       disk_backup
       borg
       backup
+      wget https://raw.githubusercontent.com/edsonsbj/Backup-Restore/main/Backup.sh
+      wget https://raw.githubusercontent.com/edsonsbj/Backup-Restore/main/Restore.sh
+      chmod 700 *.sh
+      clear
       cron
       ;;
     2)
       disk_backup
       borg
       nextcloud
+      wget https://raw.githubusercontent.com/edsonsbj/Backup-Restore/main/scripts/Nextcloud/Backup.sh
+      wget https://raw.githubusercontent.com/edsonsbj/Backup-Restore/main/scripts/Nextcloud/Restore.sh
+      chmod 700 *.sh
+      clear
       cron
       ;;
     3)
       disk_backup
       borg
-      configure_mediaserver
+      mediaserver
+      wget https://raw.githubusercontent.com/edsonsbj/Backup-Restore/main/scripts/Media%20Server/Backup.sh
+      wget https://raw.githubusercontent.com/edsonsbj/Backup-Restore/main/scripts/Media%20Server/Restore.sh
+      chmod 700 *.sh
+      clear
       cron
       ;;      
     4)
       disk_backup
       borg
-      configure_nextcloud_mediaserver
+      nextcloud
+      mediaserver
+      wget https://raw.githubusercontent.com/edsonsbj/Backup-Restore/main/scripts/Nextcloud%20%2B%20Media%20server%20/Backup.sh
+      wget https://raw.githubusercontent.com/edsonsbj/Backup-Restore/main/scripts/Nextcloud%20%2B%20Media%20server%20/Restore.sh
+      chmod 700 *.sh
+      clear
       cron
       ;;
     5)
