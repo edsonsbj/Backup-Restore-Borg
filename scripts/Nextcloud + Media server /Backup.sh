0e44f9e57b50936995f64aaeca1a1ebf9ed93d98
edsonsbj <edson_silvasp@hotmail.com>
2023-10-31 00:16:31 -0300

    Nextcloud + Media server/ scripts created

--- scripts/Nextcloud + Media server /Backup.sh
+++ scripts/Nextcloud + Media server /Backup.sh
@@ -8,6 +8,10 @@ touch "$LogFile"
 exec > >(tee -a "$LogFile")
 exec 2>&1
 
+# some helpers and error handling:
+info() { printf "\n%s %s\n\n" "$( date )" "$*" >&2; }
+trap 'echo $( date ) Backup interrupted >&2; exit 2' INT TERM
+
 ## ---------------------------------- TESTS ------------------------------ #
 
 # Check if the script is being executed by root or with sudo
@@ -16,89 +20,197 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-# Check if the removable drive is connected and mounted correctly
-if [[ $(lsblk -no uuid /dev/sd*) == *"$uuid"* ]]; then
-    echo "========== The drive is connected and mounted. =========="
-    echo ""
+device=$(blkid -U "$uuid")
+
+if [ -z "$device" ]; then
+  echo "========== The unit with UUID $uuid Is not connected. Leaving the script.=========="
+  exit 1
+fi
+
+echo "========== The unit with UUID $uuid is connected and corresponds to the device $device. =========="
+
+# Check that the unit is assembled
+if grep -qs "BackupDisk" /proc/mounts; then
+  echo "========== The unit is assembled ==========."
 else
-    echo "========== The drive is not connected or mounted. =========="
+  echo "========== The unit is not assembled. Trying to assemble...=========="
 
-    # Try to mount the drive
-    sudo mount -U $uuid $BackupDir 2>/dev/null
-
-    # Check if the drive is now mounted
-    if [[ $(lsblk -no uuid /dev/sd*) == *"$uuid"* ]]; then
-        echo "========== The drive has been successfully mounted. =========="
-        echo ""
-    else
-        echo "========== Failed to mount the drive. Exiting script. =========="
-        exit 1
-    fi
+  # Try to assemble the unit
+  if mount "$device" "BackupDisk"; then
+    echo "========== The unit was successfully assembled.=========="
+  else
+    echo "========== Failure when setting up the unit. Leaving the script.=========="
+    exit 1
+  fi
 fi
 
 # Are there write and read permissions?
-if [ ! -w "$BackupDir" ]; then
+if [ ! -w "BackupDisk" ]; then
     echo "========== No write permissions =========="
     exit 1
 fi
 
 # -------------------------------FUNCTIONS----------------------------------------- #
 
+BORG_OPTS="--verbose --filter AME --list --progress --stats --show-rc --compression lz4 --exclude-caches"
+
+# Function to Nextcloud Maintenance Mode
+nextcloud_enable() {
+    # Enabling Maintenance Mode
+	sudo -u www-data php $NextcloudConfig/occ maintenance:mode --on
+}
+
+nextcloud_disable() {
+    # Disabling Nextcloud Maintenance Mode
+	sudo -u www-data php $NextcloudConfig/occ maintenance:mode --off
+}
+
+# Function to WebServer Stop Start
+stop_webserver() {
+    # Stop Web Server
+	systemctl stop $webserverServiceName
+}
+
+start_webserver() {
+    # Stop Media Server
+    systemctl stop "$MediaserverService"
+}
+
+# Function to WebServer Stop Start
+stop_mediaserver() {
+    # Stop Media Server
+    systemctl stop "$MediaserverService"
+}
+
+start_mediaserver() {
+    # Start Media Server
+	systemctl start $webserverServiceName
+}
+
 # Function to backup Nextcloud settings
 nextcloud_settings() {
-    echo "========== Backing up Nextcloud settings... $( date ) =========="
+    echo "========== Backing up Nextcloud settings $( date )... =========="
     echo ""
 
-   	# Enabling Maintenance Mode
-	sudo -u www-data php $NextcloudConfig/occ maintenance:mode --on
+    nextcloud_enable
 
-	# Stop Web Server
-	systemctl stop $webserverServiceName
+    stop_webserver
+
+   	# Export the database.
+	mysqldump --quick -n --host=localhost $NextcloudDatabase --user=$DBUser --password=$DBPassword > "$NextcloudConfig/nextclouddb.sql"
 
     # Backup
-	sudo rsync -avhP --delete --exclude '*/data/' "$NextcloudConfig" "$BackupDir/Nextcloud"
+    borg create $BORG_OPTS ::'NextcloudConfigs-{now:%Y%m%d-%H%M}' $NextcloudConfig --exclude $NextcloudDataDir
 
-	# Export the database.
-	mysqldump --quick -n --host=localhost $NextcloudDatabase --user=$DBUser --password=$DBPassword > "$BackupDir/Nextcloud/nextclouddb_.sql"
+    backup_exit=$?
 
-	# Start Web Server
-	systemctl start $webserverServiceName
+    # Remove the database
+    rm "$NextcloudConfig/nextclouddb.sql"
 
-	# Disabling Nextcloud Maintenance Mode
-	sudo -u www-data php $NextcloudConfig/occ maintenance:mode --off
+    start_webserver
+    
+    nextcloud_disable
 }
 
 # Function to backup Nextcloud DATA folder
 nextcloud_data() {
-    echo "========== Backing up Nextcloud DATA folder... $( date )=========="
+    echo "========== Backing up Nextcloud DATA folder $( date )...=========="
     echo ""
 
-	# Enabling Maintenance Mode
-	sudo -u www-data php $NextcloudConfig/occ maintenance:mode --on
+    nextcloud_enable
 
-    # Backup
-	sudo rsync -avhP --delete --exclude '*/files_trashbin/' "$NextcloudDataDir" "$BackupDir/Nextcloud_datadir"
+    borg create $BORG_OPTS ::'NextcloudData-{now:%Y%m%d-%H%M}' $NextcloudDataDir --exclude "$NextcloudDataDir/*/files_trashbin"
 
-	# Disabling Nextcloud Maintenance Mode
-	sudo -u www-data php $NextcloudConfig/occ maintenance:mode --off
+    backup_exit=$?
+
+    nextcloud_disable
 }
 
 # Function to perform a complete Nextcloud backup
 nextcloud_complete() {
-    nextcloud_settings
-    nextcloud_data
-}
+    echo "========== Backing up Nextcloud $( date )... =========="
+    echo ""
+    
+    nextcloud_enable
 
-mediaserver_settings() {
-    echo "=============== Backing up Media Server settings... $( date)==============="
-    # Stop Media Server
-    sudo systemctl stop $MediaserverService
+    stop_webserver
+
+   	# Export the database.
+	mysqldump --quick -n --host=localhost $NextcloudDatabase --user=$DBUser --password=$DBPassword > "$NextcloudConfig/nextclouddb.sql"
 
     # Backup
-    sudo rsync -avhP --delete --exclude={'*/Cache','*/cache','*/Crash Reports','*/Diagnostics','*/Logs','*/logs','*/transcoding-temp'} "$MediaserverConf" "$BackupDir/Mediaserver"
+    borg create $BORG_OPTS ::'NextcloudFull-{now:%Y%m%d-%H%M}' $NextcloudConfig $NextcloudDataDir --exclude "$NextcloudDataDir/*/files_trashbin"
 
-    # Start Media Server
-    sudo systemctl start $MediaserverService
+    backup_exit=$?
+
+    # Remove the database
+    rm "$NextcloudConfig/nextclouddb.sql"
+
+    start_webserver
+
+    nextcloud_disable
+}
+
+# Function to backup Nextcloud and Media Server settings
+nextcloud_mediaserver_settings() {
+    BORG_EXCLUDE="--exclude '$NextcloudDataDir' --exclude '$MediaserverConf/Cache*' --exclude '$MediaserverConf/cache' --exclude '$MediaserverConf/Crash Reports' --exclude '$MediaserverConf/Diagnostics' --exclude '$MediaserverConf/Logs' --exclude '$MediaserverConf/logs' --exclude '$MediaserverConf/transcoding-temp'"
+
+    echo "========== Backing up Nextcloud and Media Server settings $( date )... =========="
+    echo ""
+
+    nextcloud_enable
+
+    stop_webserver
+
+    stop_mediaserver
+
+   	# Export the database.
+	mysqldump --quick -n --host=localhost $NextcloudDatabase --user=$DBUser --password=$DBPassword > "$NextcloudConfig/nextclouddb.sql"
+
+    # Backup
+    borg create $BORG_OPTS $BORG_EXCLUDE ::'SettingsServer-{now:%Y%m%d-%H%M}' "$NextcloudConfig" "$MediaserverConf"
+
+    backup_exit=$?
+
+    # Remove the database
+    rm "$NextcloudConfig/nextclouddb.sql"
+
+    nextcloud_disable
+
+    start_webserver
+
+    start_mediaserver
+}
+
+# Function to perform a complete Nextcloud and Media Server Settings backup
+nextcloud_mediaserver_complete() {
+    BORG_EXCLUDE=" --exclude '$NextcloudDataDir/*/files_trashbin' --exclude '$MediaserverConf/Cache*' --exclude '$MediaserverConf/cache' --exclude '$MediaserverConf/Crash Reports' --exclude '$MediaserverConf/Diagnostics' --exclude '$MediaserverConf/Logs' --exclude '$MediaserverConf/logs' --exclude '$MediaserverConf/transcoding-temp'"
+
+    echo "========== Backing up Nextcloud and Media Server $( date )... =========="
+    echo ""
+    
+    nextcloud_enable
+
+    stop_webserver
+
+    stop_mediaserver
+
+   	# Export the database.
+	mysqldump --quick -n --host=localhost $NextcloudDatabase --user=$DBUser --password=$DBPassword > "$NextcloudConfig/nextclouddb.sql"
+
+    # Backup
+    borg create $BORG_OPTS $BORG_EXCLUDE ::'NextcloudFull-{now:%Y%m%d-%H%M}' "$NextcloudConfig" "$NextcloudDataDir" "$MediaserverConf"
+
+    backup_exit=$?
+
+    # Remove the database
+    rm "$NextcloudConfig/nextclouddb.sql"
+
+    start_webserver
+
+    nextcloud_disable
+
+    start_mediaserver
 }
 
 # Check if an option was passed as an argument
@@ -106,33 +218,30 @@ if [[ ! -z $1 ]]; then
     # Execute the corresponding Backup option
     case $1 in
         1)
-            nextcloud_complete
+            nextcloud_settings
             ;;
         2)
-            nextcloud_settings
-            ;;
-        3)
             nextcloud_data
             ;;
+        3)
+            nextcloud_complete
+            ;;
         4)
-            nextcloud_settings
-            mediaserver_settings
+            nextcloud_mediaserver_settings
             ;;
         5)
-            nextcloud_complete
-            mediaserver_settings
-            ;;            
+            nextcloud_mediaserver_complete
+            ;;               
         *)
             echo "Invalid option!"
             ;;
     esac
-
 else
     # Display the menu to choose the Backup option
     echo "Choose a Backup option:"
-    echo "1. Backup Nextcloud configurations, database, and data folder."
-    echo "2. Backup Nextcloud configurations and database."
-    echo "3. Backup only the Nextcloud data folder. Useful if the folder is stored elsewhere."
+    echo "1. Backup Nextcloud configurations and database."
+    echo "2. Backup only the Nextcloud data folder. Useful if the folder is stored elsewhere."
+    echo "3. Backup Nextcloud configurations, database, and data folder."
     echo "4. Backup Nextcloud and Media Server Settings."
     echo "5. Backup Nextcloud settings, database and data folder, as well as Media Server settings."
     echo "6. To go out."
@@ -143,22 +252,20 @@ else
     # Execute the corresponding Backup option
     case $option in
         1)
-            nextcloud_complete
+            nextcloud_settings
             ;;
         2)
-            nextcloud_settings
-            ;;
-        3)
             nextcloud_data
             ;;
+        3)
+            nextcloud_complete
+            ;;
         4)
-            nextcloud_settings
-            mediaserver_settings
+            nextcloud_mediaserver_settings
             ;;
         5)
-            nextcloud_complete
-            mediaserver_settings
-            ;;            
+            nextcloud_mediaserver_complete
+            ;;             
         6)
             echo "Leaving the script."
             exit 0
@@ -169,10 +276,32 @@ else
     esac
 fi
 
+    info "Pruning repository"
+
+    # Use the subcoming `prune` to keep 7 days, 4 per week and 6 per month
+    # files of this machine.The prefix '{hostname}-' is very important for
+    # limits PLA's operation to files in this machine and does not apply to
+    # Files of other machines too:
+
+    borg prune --list --progress --show-rc --keep-daily 7 --keep-weekly 4 --keep-monthly 6
+
+    prune_exit=$? 
+
 # Worked well? Unmount.
 if [ "$?" = "0" ]; then
-    echo "===============#### Backup completed. The removable drive has been unmounted and powered off. ===============#" >> "$LogFile"
+    echo ""
+    echo "========== Backup completed. The removable drive has been unmounted and powered off. =========="
     umount "/dev/disk/by-uuid/$uuid"
-    sudo udisksctl power-off -b "/dev/disk/by-uuid/$uuid" >> "$LogFile"
-    exit 0
+    sudo udisksctl power-off -b "/dev/disk/by-uuid/$uuid"
+fi
+
+# use highest exit code as global exit code
+global_exit=$(( backup_exit > prune_exit ? backup_exit : prune_exit ))
+
+if [ ${global_exit} -eq 0 ]; then
+    info "Backup, Prune finished successfully" 2>&1 | tee -a
+elif [ ${global_exit} -eq 1 ]; then
+    info "Backup, Prune finished with warnings" 2>&1 | tee -a
+else
+    info "Backup, Prune finished with errors" 2>&1 | tee -a
 fi
--- scripts/Nextcloud + Media server /Restore.sh
+++ scripts/Nextcloud + Media server /Restore.sh
@@ -8,6 +8,9 @@ touch "$LogFile"
 exec > >(tee -a "$LogFile")
 exec 2>&1
 
+# Function for error messages
+errorecho() { cat <<< "$@" 1>&2; } 
+
 ## ---------------------------------- TESTS ------------------------------ #
 
 # Check if the script is being executed by root or with sudo
@@ -16,145 +19,304 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-# Check if the removable drive is connected and mounted correctly
-if [[ $(lsblk -no uuid /dev/sd*) == *"$uuid"* ]]; then
-    echo "========== The drive is connected and mounted. =========="
-    echo ""
+device=$(blkid -U "$uuid")
+
+if [ -z "$device" ]; then
+  echo "========== The unit with UUID $uuid Is not connected. Leaving the script.=========="
+  exit 1
+fi
+
+echo "========== The unit with UUID $uuid is connected and corresponds to the device $device. =========="
+
+# Check that the unit is assembled
+if grep -qs "BackupDisk" /proc/mounts; then
+  echo "========== The unit is assembled ==========."
 else
-    echo "========== The drive is not connected or mounted. =========="
+  echo "========== The unit is not assembled. Trying to assemble...=========="
 
-    # Try to mount the drive
-    sudo mount -U $uuid $BackupDir 2>/dev/null
-
-    # Check if the drive is now mounted
-    if [[ $(lsblk -no uuid /dev/sd*) == *"$uuid"* ]]; then
-        echo "========== The drive has been successfully mounted. =========="
-        echo ""
-    else
-        echo "========== Failed to mount the drive. Exiting script. =========="
-        exit 1
-    fi
+  # Try to assemble the unit
+  if mount "$device" "BackupDisk"; then
+    echo "========== The unit was successfully assembled.=========="
+  else
+    echo "========== Failure when setting up the unit. Leaving the script.=========="
+    exit 1
+  fi
 fi
 
 # Are there write and read permissions?
-if [ ! -w "$BackupDir" ]; then
+if [ ! -w "BackupDisk" ]; then
     echo "========== No write permissions =========="
     exit 1
 fi
 
 # -------------------------------FUNCTIONS----------------------------------------- #
 
+# Function to Nextcloud Maintenance Mode
+nextcloud_enable() {
+    # Enabling Maintenance Mode
+	sudo -u www-data php $NextcloudConfig/occ maintenance:mode --on
+}
+
+nextcloud_disable() {
+    # Disabling Nextcloud Maintenance Mode
+	sudo -u www-data php $NextcloudConfig/occ maintenance:mode --off
+}
+
+# Function to WebServer Stop Start
+stop_webserver() {
+    # Stop Web Server
+	systemctl stop $webserverServiceName
+}
+
+start_webserver() {
+    # Stop Web Server
+	systemctl start $webserverServiceName
+}
+
+# Function to WebServer Stop Start
+stop_mediaserver() {
+    # Stop Media Server
+    systemctl stop "$MediaserverService"
+}
+
+start_mediaserver() {
+    # Start Media Server
+	systemctl start $MediaserverService
+}
+
+# Obtaining file information and dates to be restored
+check_restore() {
+    # Change to the root directory. This is critical because borg extract uses relative directory, so we must change to the root of the system to avoid errors or random directories during restoration.
+
+    echo "Changing to the root directory..."
+    cd /
+    echo "pwd is $(pwd)"
+    echo "location of the database backup file is " '/'
+
+    if [ $? -eq 0 ]; then
+        echo "Done"
+    else
+        echo "Failed to change to the root directory. Restoration failed."
+        exit 1
+    fi
+
+    # Check if the restoration date is specified
+    if [ -z "$ARCHIVE_DATE" ]
+    then
+        echo "Please specify the restoration date."
+        exit 1
+    fi
+
+    # Find the backup file name corresponding to the specified date
+    ARCHIVE_NAME=$(borg list $BORG_REPO | grep $ARCHIVE_DATE | awk '{print $1}')
+
+    # Check if the backup file is found
+    if [ -z "$ARCHIVE_NAME" ]
+    then
+        echo "Could not find a backup file for the specified date: $ARCHIVE_DATE"
+        exit 1
+    fi
+
+}
+
 # Function to restore Nextcloud settings
 nextcloud_settings() {
-    echo "=============== Restoring Nextcloud settings...$( date )==============="
+    echo "========== Restoring Nextcloud settings $( date )... =========="
     echo ""
 
-   	# Enabling Maintenance Mode
-	sudo -u www-data php $NextcloudConfig/occ maintenance:mode --on
+    check_restore
 
-	# Stop Web Server
-	systemctl stop $webserverServiceName
+    nextcloud_enable
 
-	# Remove the current Nextcloud folder
-	mv "$NextcloudConfig" "$NextcloudConfig.bk"
+    stop_webserver
 
-    # Restore
-	sudo rsync -avhP "$BackupDir/Nextcloud" "$NextcloudConfig"
+    # Removing old versions 
+    mv $NextcloudConfig '$NextcloudConfig.old/'
 
-	# Restore permissions
-	chmod -R 755 $NextcloudConfig
-	chown -R www-data:www-data $NextcloudConfig
+    # Extract Files
+    borg extract -v --list $BORG_REPO::$ARCHIVE_NAME $NextcloudConfig
 
-	# Export the database.
-	mysql -u --host=localhost --user=$DBUser --password=$PDBPassword $NextcloudDatabase < "$BackupDir/Nextcloud/nextclouddb.sql"
+    # Restore the database
+    mysql -u --host=localhost --user=$DBUser --password=$DBPassword $NextcloudDatabase < "$NextcloudConfig/nextclouddb.sql" >> $RESTLOGFILE_PATH
 
-	# Start Web Server
-	systemctl start $webserverServiceName
+    # Restore permissions
+    chmod -R 755 $NextcloudConfig
+    chown -R www-data:www-data $NextcloudConfig
 
-	# Disabling Nextcloud Maintenance Mode
-	sudo -u www-data php $NextcloudConfig/occ maintenance:mode --off
+    # Removing unnecessary files
+    rm "$NextcloudConfig/nextclouddb.sql"
+    
+    start_webserver
+
+    nextcloud_disable
 }
 
 # Function to restore Nextcloud DATA folder
 nextcloud_data() {
-    echo "=============== Restoring Nextcloud DATA folder...$( date )==============="
+    echo "========== Restoring Nextcloud DATA folder $( date )...=========="
     echo ""
 
-	# Enabling Maintenance Mode
-	sudo -u www-data php $NextcloudConfig/occ maintenance:mode --on
+    check_restore
 
-    # Restore
-	sudo rsync -avhP "$BackupDir/Nextcloud_datadir" "$NextcloudDataDir"
+    nextcloud_enable
 
-	# Restore permissions
-	chmod -R 770 $NextcloudDataDir
-	chown -R www-data:www-data $NextcloudDataDir
-
-	# Disabling Nextcloud Maintenance Mode
-	sudo -u www-data php $NextcloudConfig/occ maintenance:mode --off
-}
-
-# Function to perform a complete Nextcloud restore
-nextcloud_complete() {
-    nextcloud_settings
-    nextcloud_data
-}
-
-mediaserver_settings() {
-    echo "=============== Backing up Media Server settings...==============="
-    
-    # Remove the current directory from Media Derver
-    mv "$MediaserverConf" "$MediaserverConf.bk"
-
-    # Stop Media Server
-    sudo systemctl stop $MediaserverService
-
-    # Restore
-    sudo rsync -avhP "$BackupDir/Mediaserver" "$MediaserverConf"
-
-    # Start Media Server
-    sudo systemctl start $MediaserverService
+    # Extract Files
+    borg extract -v --list $BORG_REPO::$ARCHIVE_NAME $NextcloudDataDir
 
     # Restore permissions
+    chmod -R 770 $NextcloudDataDir 
+    chown -R www-data:www-data $NextcloudDataDir
+
+    nextcloud_disable
+}
+
+# Function to restore Nextcloud 
+nextcloud_complete() {
+    echo "========== Restoring Nextcloud $( date )... =========="
+    echo ""
+
+    check_restore
+
+    nextcloud_enable
+
+    stop_webserver
+
+    # Removing old versions 
+    mv $NextcloudConfig '$NextcloudConfig.old/'
+
+    # Extract Files
+    borg extract -v --list $BORG_REPO::$ARCHIVE_NAME $NextcloudConfig $NextcloudDataDir
+
+    # Restore the database
+    mysql -u --host=localhost --user=$DBUser --password=$DBPassword $NextcloudDatabase < "$NextcloudConfig/nextclouddb.sql" >> $RESTLOGFILE_PATH
+
+    # Restore permissions
+    chmod -R 755 $NextcloudConfig
+    chown -R www-data:www-data $NextcloudConfig
+    chmod -R 770 $NextcloudDataDir 
+    chown -R www-data:www-data $NextcloudDataDir
+
+    # Removing unnecessary files
+    rm "$NextcloudConfig/nextclouddb.sql"
+
+    start_webserver
+
+    nextcloud_disable
+}
+
+# Function to restore Nextcloud and Media Server settings
+nextcloud_mediaserver_settings() {
+    echo "========== Restoring Nextcloud settings $( date )... =========="
+    echo ""
+
+    check_restore
+
+    nextcloud_enable
+
+    stop_webserver
+
+    stop_mediaserver
+
+    # Removing old versions 
+    mv $NextcloudConfig '$NextcloudConfig.old/'
+    mv "$MediaserverConf" "$MediaserverConf.old/"
+
+    # Extract Files
+    borg extract -v --list $BORG_REPO::$ARCHIVE_NAME $NextcloudConfig "$MediaserverConf"
+
+    # Restore the database
+    mysql -u --host=localhost --user=$DBUser --password=$DBPassword $NextcloudDatabase < "$NextcloudConfig/nextclouddb.sql" >> $RESTLOGFILE_PATH
+
+    # Restore permissions
+    chmod -R 755 $NextcloudConfig
+    chown -R www-data:www-data $NextcloudConfig
     chmod -R 755 $MediaserverConf
     chown -R $MediaserverUser:$MediaserverUser $MediaserverConf
 
     # Add the Media Server User to the www-data group to access Nextcloud folders
     sudo adduser $MediaserverUser www-data
+
+    start_webserver
+
+    start_mediaserver
+
+    nextcloud_disable
+
+    # Removing unnecessary files
+    rm "$NextcloudConfig/nextclouddb.sql"
+}
+
+# Function to perform a complete Nextcloud and Media Server Settings restore
+nextcloud_mediaserver_complete() {
+    echo "========== Restoring Nextcloud settings $( date )... =========="
+    echo ""
+
+    check_restore
+
+    nextcloud_enable
+
+    stop_webserver
+
+    stop_mediaserver
+
+    # Removing old versions 
+    mv $NextcloudConfig '$NextcloudConfig.old/'
+    mv "$MediaserverConf" "$MediaserverConf.old/"
+
+    # Extract Files
+    borg extract -v --list $BORG_REPO::$ARCHIVE_NAME "$NextcloudConfig" "$NextcloudDataDir" "$MediaserverConf"
+
+    # Restore the database
+    mysql -u --host=localhost --user=$DBUser --password=$DBPassword $NextcloudDatabase < "$NextcloudConfig/nextclouddb.sql" >> $RESTLOGFILE_PATH
+
+    # Restore permissions
+    chmod -R 755 $NextcloudConfig
+    chown -R www-data:www-data $NextcloudConfig
+    chmod -R 755 $MediaserverConf
+    chown -R $MediaserverUser:$MediaserverUser $MediaserverConf
+
+    # Add the Media Server User to the www-data group to access Nextcloud folders
+    sudo adduser $MediaserverUser www-data
+
+    start_webserver
+
+    start_mediaserver
+
+    nextcloud_disable
+
+    # Removing unnecessary files
+    rm "$NextcloudConfig/nextclouddb.sql"   
 }
 
 # Check if an option was passed as an argument
 if [[ ! -z $1 ]]; then
-    # Execute the corresponding restore option
+    # Execute the corresponding Restore option
     case $1 in
         1)
-            nextcloud_complete
+            nextcloud_settings
             ;;
         2)
-            nextcloud_settings
-            ;;
-        3)
             nextcloud_data
             ;;
+        3)
+            nextcloud_complete
+            ;;
         4)
-            nextcloud_settings
-            mediaserver_settings
+            nextcloud_mediaserver_settings
             ;;
         5)
-            nextcloud_complete
-            mediaserver_settings
-            ;;           
+            nextcloud_mediaserver_complete
+            ;;               
         *)
             echo "Invalid option!"
             ;;
     esac
-
 else
     # Display the menu to choose the Restore option
     echo "Choose a Restore option:"
-    echo "1. Restore Nextcloud configurations, database, and data folder."
-    echo "2. Restore Nextcloud configurations and database."
-    echo "3. Restore only the Nextcloud data folder. Useful if the folder is stored elsewhere."
+    echo "1. Restore Nextcloud configurations and database."
+    echo "2. Restore only the Nextcloud data folder. Useful if the folder is stored elsewhere."
+    echo "3. Restore Nextcloud configurations, database, and data folder."
     echo "4. Restore Nextcloud and Media Server Settings."
     echo "5. Restore Nextcloud settings, database and data folder, as well as Media Server settings."
     echo "6. To go out."
@@ -162,37 +324,29 @@ else
     # Read the option entered by the user
     read option
 
-    # Execute the corresponding restore option
+    # Execute the corresponding Restore option
     case $option in
         1)
-            nextcloud_complete
+            nextcloud_settings
             ;;
         2)
-            nextcloud_settings
-            ;;
-        3)
             nextcloud_data
             ;;
+        3)
+            nextcloud_complete
+            ;;
         4)
-            nextcloud_settings
-            mediaserver_settings
+            nextcloud_mediaserver_settings
             ;;
         5)
-            nextcloud_complete
-            mediaserver_settings
-            ;;            
+            nextcloud_mediaserver_complete
+            ;;             
         6)
             echo "Leaving the script."
             exit 0
-            ;; 
+            ;;            
+        *)
+            echo "Invalid option!"
+            ;;
     esac
 fi
-
-  # Worked well? Unmount.
-  [ "$?" = "0" ] && {
-    echo "===============#### Restore completed. The removable drive has been unmounted and powered off. ===============#"
- 	eval umount /dev/disk/by-uuid/$uuid
-	eval sudo udisksctl power-off -b /dev/disk/by-uuid/$uuid >>$LogFile
-    exit 0
-  }
-}
