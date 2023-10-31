47b55fff32a91606d6c8688e2960909950cb8df4
edsonsbj <edson_silvasp@hotmail.com>
2023-10-30 22:24:13 -0300

    Create Backup.sh and Restore.sh

--- Backup.sh
+++ Backup.sh
@@ -8,6 +8,10 @@ touch "$LogFile"
 exec > >(tee -a "$LogFile")
 exec 2>&1
 
+# some helpers and error handling:
+info() { printf "\n%s %s\n\n" "$( date )" "$*" >&2; }
+trap 'echo $( date ) Backup interrupted >&2; exit 2' INT TERM
+
 ## ---------------------------------- TESTS ------------------------------ #
 
 # Check if the script is being executed by root or with sudo
@@ -16,45 +20,65 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-# Check if the removable drive is connected and mounted correctly
-if [[ $(lsblk -no uuid /dev/sd*) == *"$uuid"* ]]; then
-    echo "========== The drive is connected and mounted. =========="
-    echo ""
+device=$(blkid -U "$uuid")
+
+if [ -z "$device" ]; then
+  echo "========== The unit with UUID $uuid Is not connected. Leaving the script.=========="
+  exit 1
+fi
+
+echo "========== The unit with UUID $uuid is connected and corresponds to the device $device. =========="
+
+# Check that the unit is assembled
+if grep -qs "$MountPoint" /proc/mounts; then
+  echo "========== The unit is assembled ==========."
 else
-    echo "========== The drive is not connected or mounted. =========="
+  echo "========== The unit is not assembled. Trying to assemble...=========="
 
-    # Try to mount the drive
-    sudo mount -U $uuid $BackupDir 2>/dev/null
-
-    # Check if the drive is now mounted
-    if [[ $(lsblk -no uuid /dev/sd*) == *"$uuid"* ]]; then
-        echo "========== The drive has been successfully mounted. =========="
-        echo ""
-    else
-        echo "========== Failed to mount the drive. Exiting script. =========="
-        exit 1
-    fi
+  # Try to assemble the unit
+  if mount "$device" "$MountPoint"; then
+    echo "========== The unit was successfully assembled.=========="
+  else
+    echo "========== Failure when setting up the unit. Leaving the script.=========="
+    exit 1
+  fi
 fi
 
 # Are there write and read permissions?
-if [ ! -w "$BackupDir" ]; then
+if [ ! -w "$MountPoint" ]; then
     echo "========== No write permissions =========="
     exit 1
 fi
 
 ## -------------------------- MAIN SCRIPT -------------------------- #
 
-echo "Starting Backup $(date)..."
+BORG_OPTS="--verbose --filter AME --list --progress --stats --show-rc --compression lz4 --exclude-caches"
+BORG_EXCLUDE="--exclude '/dev' --exclude '/proc' --exclude '/sys' --exclude '/tmp' --exclude '/run' --exclude '/mnt' --exclude '/media' --exclude '/lost+found'"
 
 # Function to backup
 backup() {
-    echo "Backing up ..."
+    echo "========== Backing up $( date )... =========="
+    echo ""
 
     # Backup
-	sudo rsync -avhP --delete --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} "$SourceDir" "$BackupDir"
+    borg create $BORG_OPTS $BORG_EXCLUDE ::'Full-{now:%Y%m%d-%H%M}' "$SourceDir"
+
+    backup_exit=$?
+
+    info "Pruning repository"
+
+    # Use the subcoming `prune` to keep 7 days, 4 per week and 6 per month
+    # files of this machine.The prefix '{hostname}-' is very important for
+    # limits PLA's operation to files in this machine and does not apply to
+    # Files of other machines too:
+
+    borg prune --list --progress --show-rc --keep-daily 7 --keep-weekly 4 --keep-monthly 6
+
+    prune_exit=$? 
 
     # Worked well? Unmount.
     if [ $? -eq 0 ]; then
+        echo ""
         echo "Backup completed. The removable drive has been unmounted and powered off."
         umount "/dev/disk/by-uuid/$uuid"
         sudo udisksctl power-off -b "/dev/disk/by-uuid/$uuid"
@@ -64,3 +88,14 @@ backup() {
 
 # Call the backup function
 backup
+
+# use highest exit code as global exit code
+global_exit=$(( backup_exit > prune_exit ? backup_exit : prune_exit ))
+
+if [ ${global_exit} -eq 0 ]; then
+    info "Backup, Prune finished successfully" 2>&1 | tee -a
+elif [ ${global_exit} -eq 1 ]; then
+    info "Backup, Prune finished with warnings" 2>&1 | tee -a
+else
+    info "Backup, Prune finished with errors" 2>&1 | tee -a
+fi
--- Restore.sh
+++ Restore.sh
@@ -8,6 +8,9 @@ touch "$LogFile"
 exec > >(tee -a "$LogFile")
 exec 2>&1
 
+# Function for error messages
+errorecho() { cat <<< "$@" 1>&2; } 
+
 ## ---------------------------------- TESTS ------------------------------ #
 
 # Check if the script is being executed by root or with sudo
@@ -16,51 +19,92 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-# Check if the removable drive is connected and mounted correctly
-if [[ $(lsblk -no uuid /dev/sd*) == *"$uuid"* ]]; then
-    echo "========== The drive is connected and mounted. =========="
-    echo ""
+device=$(blkid -U "$uuid")
+
+if [ -z "$device" ]; then
+  echo "========== The unit with UUID $uuid Is not connected. Leaving the script.=========="
+  exit 1
+fi
+
+echo "========== The unit with UUID $uuid is connected and corresponds to the device $device. =========="
+
+# Check that the unit is assembled
+if grep -qs "$MountPoint" /proc/mounts; then
+  echo "========== The unit is assembled ==========."
 else
-    echo "========== The drive is not connected or mounted. =========="
+  echo "========== The unit is not assembled. Trying to assemble...=========="
 
-    # Try to mount the drive
-    sudo mount -U $uuid $BackupDir 2>/dev/null
-
-    # Check if the drive is now mounted
-    if [[ $(lsblk -no uuid /dev/sd*) == *"$uuid"* ]]; then
-        echo "========== The drive has been successfully mounted. =========="
-        echo ""
-    else
-        echo "========== Failed to mount the drive. Exiting script. =========="
-        exit 1
-    fi
+  # Try to assemble the unit
+  if mount "$device" "$MountPoint"; then
+    echo "========== The unit was successfully assembled.=========="
+  else
+    echo "========== Failure when setting up the unit. Leaving the script.=========="
+    exit 1
+  fi
 fi
 
 # Are there write and read permissions?
-if [ ! -w "$BackupDir" ]; then
+if [ ! -w "$MountPoint" ]; then
     echo "========== No write permissions =========="
     exit 1
 fi
 
-## -------------------------- MAIN SCRIPT -------------------------- #
+# -------------------------------FUNCTIONS----------------------------------------- #
 
-echo "Starting Restore $(date)..."
+# Obtaining file information and dates to be restored
+check_restore() {
+    # Change to the root directory. This is critical because borg extract uses relative directory, so we must change to the root of the system to avoid errors or random directories during restoration.
 
-# Function to Restore
-restore() {
-    echo "Restoring ..."
+    echo "Changing to the root directory..."
+    cd /
+    echo "pwd is $(pwd)"
+    echo "location of the database backup file is " '/'
 
-    # Restore
-	sudo rsync -avhP "$BackupDir" "$SourceDir"
+    if [ $? -eq 0 ]; then
+        echo "Done"
+    else
+        echo "Failed to change to the root directory. Restoration failed."
+        exit 1
+    fi
+
+    # Check if the restoration date is specified
+    if [ -z "$ARCHIVE_DATE" ]
+    then
+        echo "Please specify the restoration date."
+        exit 1
+    fi
+
+    # Find the backup file name corresponding to the specified date
+    ARCHIVE_NAME=$(borg list $BORG_REPO | grep $ARCHIVE_DATE | awk '{print $1}')
+
+    # Check if the backup file is found
+    if [ -z "$ARCHIVE_NAME" ]
+    then
+        echo "Could not find a backup file for the specified date: $ARCHIVE_DATE"
+        exit 1
+    fi
+
+}
+
+# Function to Restore 
+Restore() {
+    echo "========== Restoring settings $( date )... =========="
+    echo ""
+
+    check_restore
+
+    # Extract Files
+    borg extract -v --list $BORG_REPO::$ARCHIVE_NAME "$SourceDir"
+}
 
     # Worked well? Unmount.
     if [ $? -eq 0 ]; then
+        echo ""
         echo "Restore completed. The removable drive has been unmounted and powered off."
         umount "/dev/disk/by-uuid/$uuid"
         sudo udisksctl power-off -b "/dev/disk/by-uuid/$uuid"
         exit 0
     fi
-}
 
 # Call the restore function
 restore
