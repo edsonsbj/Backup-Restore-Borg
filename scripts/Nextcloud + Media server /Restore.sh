11b26b904e0a9816f816091b8fb6ba885e29d3a6
edsonsbj <edson_silvasp@hotmail.com>
2023-10-31 00:27:01 -0300

    Add umount disk Restore.sh

--- scripts/Nextcloud + Media server /Restore.sh
+++ scripts/Nextcloud + Media server /Restore.sh
@@ -350,3 +350,11 @@ else
             ;;
     esac
 fi
+
+# Worked well? Unmount.
+if [ "$?" = "0" ]; then
+    echo ""
+    echo "========== Backup completed. The removable drive has been unmounted and powered off. =========="
+    umount "/dev/disk/by-uuid/$uuid"
+    sudo udisksctl power-off -b "/dev/disk/by-uuid/$uuid"
+fi
