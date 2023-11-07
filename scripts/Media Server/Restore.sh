4db2e0fe0ad3cf891fdec2d8d7d46a980e66b880
edsonsbj <edson_silvasp@hotmail.com>
2023-11-07 13:58:27 -0300

    Update Restore.sh

--- scripts/Media Server/Restore.sh
+++ scripts/Media Server/Restore.sh
@@ -3,6 +3,8 @@
 CONFIG="$(dirname "${BASH_SOURCE[0]}")/BackupRestore.conf"
 . $CONFIG
 
+ARCHIVE_DATE=$2
+
 # Create a log file to record command outputs
 touch "$LogFile"
 exec > >(tee -a "$LogFile")
@@ -12,11 +14,19 @@ exec 2>&1
 errorecho() { cat <<< "$@" 1>&2; } 
 
 ## ---------------------------------- TESTS ------------------------------ #
-
 # Check if the script is being executed by root or with sudo
 if [[ $EUID -ne 0 ]]; then
    echo "========== This script needs to be executed as root or with sudo. ==========" 
    exit 1
+fi
+
+# Change to the root directory, and exit with an error message if it fails
+if cd /; then
+    echo "Changed to the root directory ($(pwd))"
+    echo "Location of the database backup file is /"
+else
+    echo "Failed to change to the root directory. Restoration failed."
+    exit 1
 fi
 
 device=$(blkid -U "$uuid")
@@ -50,31 +60,18 @@ if [ ! -w "$BackupDisk" ]; then
 fi
 
 # -------------------------------FUNCTIONS----------------------------------------- #
-
 # Obtaining file information and dates to be restored
 check_restore() {
-    # Change to the root directory. This is critical because borg extract uses relative directory, so we must change to the root of the system to avoid errors or random directories during restoration.
-
-    echo "Changing to the root directory..."
-    cd /
-    echo "pwd is $(pwd)"
-    echo "location of the database backup file is " '/'
-
-    if [ $? -eq 0 ]; then
-        echo "Done"
-    else
-        echo "Failed to change to the root directory. Restoration failed."
-        exit 1
-    fi
-
-    ARCHIVE_DATE=$1
-
     # Check if the restoration date is specified
     if [ -z "$ARCHIVE_DATE" ]
+    then        
+        read -p "Enter the restoration date (YYYY-MM-DD): " ARCHIVE_DATE
+    if [ -z "$ARCHIVE_DATE" ]
     then
-        echo "Please specify the restoration date."
+        echo "No date provided. Going off script."
         exit 1
     fi
+ fi
 
     # Find the backup file name corresponding to the specified date
     ARCHIVE_NAME=$(borg list $BORG_REPO | grep $ARCHIVE_DATE | awk '{print $1}')
@@ -90,9 +87,6 @@ check_restore() {
 
 # Function to Restore 
 Restore() {
-    echo "========== Restoring Media Server settings $( date )... =========="
-    echo ""
-
     check_restore
 
     # Stop Media Server
@@ -100,6 +94,9 @@ Restore() {
 
     # Remove the current directory from Media Derver
     mv "$MediaserverConf" "$MediaserverConf.bk"
+
+    echo "========== Restoring Media Server settings $( date )... =========="
+    echo ""
 
     # Extract Files
     borg extract -v --list $BORG_REPO::$ARCHIVE_NAME "$MediaserverConf"
@@ -128,4 +125,4 @@ Restore() {
     fi
 
 # Call the restore function
-restore
+Restore $2
